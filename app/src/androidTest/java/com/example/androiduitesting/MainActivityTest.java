608f965f99661825f1dc8dc6f8ceedfe402ce6d3
andyjianzhou <andyzhou727@gmail.com>
2025-10-24 16:17:16 -0600

    Created cities succesfully, and added unit tests

--- app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
+++ app/src/androidTest/java/com/example/androiduitesting/MainActivityTest.java
@@ -1,7 +1,54 @@
 package com.example.androiduitesting;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MainActivityTest {
-    
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void testActivitySwitch() {
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.editText_name)).perform(typeText("Toronto"), closeSoftKeyboard());
+        onView(withId(R.id.button_confirm)).perform(click());
+        onView(withText("Toronto")).perform(click());
+        onView(withId(R.id.city_name_text)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testCityNameConsistency() {
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.editText_name)).perform(typeText("Vancouver"), closeSoftKeyboard());
+        onView(withId(R.id.button_confirm)).perform(click());
+        onView(withText("Vancouver")).perform(click());
+        onView(withId(R.id.city_name_text)).check(matches(withText("Vancouver")));
+    }
+
+    @Test
+    public void testBackButton() {
+        onView(withId(R.id.button_add)).perform(click());
+        onView(withId(R.id.editText_name)).perform(typeText("Montreal"), closeSoftKeyboard());
+        onView(withId(R.id.button_confirm)).perform(click());
+        onView(withText("Montreal")).perform(click());
+        onView(withId(R.id.back_button)).perform(click());
+        onView(withId(R.id.city_list)).check(matches(isDisplayed()));
+    }
 }
---
+++ app/src/main/java/com/example/androiduitesting/ShowActivity.java
@@ -0,0 +1,34 @@
+package com.example.androiduitesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ShowActivity extends AppCompatActivity {
+    TextView cityNameText;
+    Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+
+        cityNameText = findViewById(R.id.city_name_text);
+        backButton = findViewById(R.id.back_button);
+
+        Intent intent = getIntent();
+        String cityName = intent.getStringExtra("CITY_NAME");
+        cityNameText.setText(cityName);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
